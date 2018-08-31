a905f079782606347bac4508ddc68661b7d0adb7
jyorames <jyorames@WKWIN6490555.global.publicisgroupe.net>
2018-08-31 10:58:48 +0530

    Aditya is stupid af

---
+++ New folder/Git/src/somepackage/SomeClass.java
@@ -0,0 +1,13 @@
+package somepackage;
+
+public class SomeClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Something");
+		System.out.println("Something");
+		
+
+	}
+
+}
